b692e633e3108bb55c79c3250e182a1dfd24c0f5
Bigpapaasg <shreeharibharadwaj0@gmail.com>
2020-05-07 23:33:57 +0530

    mongo test

---
+++ Python/pymongo/mongotest.py
@@ -0,0 +1,9 @@
+import pymongo
+from pymongo import MongoClient
+cluster = MongoClient('mongodb+srv://bigpapaasg:shinchan11@cluster0-0hiha.mongodb.net/test?retryWrites=true&w=majority')
+db = cluster["test"]
+collection = db["test"]
+post = {
+    "_id":0,"name":"papa","score":5
+}
+collection.insert_one(post)
