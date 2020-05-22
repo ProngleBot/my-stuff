7117fd18ff9161c71a2350d8c77ba33caa38d85c
bigpapasg <callmeasg69@gmail.com>
2020-05-22 20:48:40 +0530

    oh

--- Python/scripts/limey1.py
+++ Python/scripts/limey1.py
@@ -1,6 +1,26 @@
-import requests
-import pprint as pp
-requests = requests.get('https://api.giphy.com/v1/gifs/random?api_key=wa7IN1SXwDMSS4vLDuNyacpMx6iNXSeP&tag=weather&rating=G')
-thumbnail = requests.json()
-thumbnail = thumbnail['data']['images']['original']['url']
-pp.pprint(thumbnail)
+import requests,time,pyautogui
+ask = input('Enter tag here: ').lower()
+x = -1
+def neko():
+    f = open("test.html",'w+')
+    f.write(f'<body bgcolor = pink>\n <h1 align = "center"><u>{ask}</u></h1><hr>')
+    for _ in range(12):
+        r = requests.get(f'https://nekos.life/api/v2/img/{ask}')
+        r = r.json()
+        r = r['url']
+        f.write(f'<img heignt ="800", width ="400", src="{r}"/>\n')
+    f.write('</body>')
+    print('done')
+    f.close()
+    pyautogui.press('f5')
+while True:
+    x = x
+    x = x+1
+    if x <= 1:
+        print('reset once')
+        neko()
+    else:
+        print(f'reset {x} times')
+        neko()
+    time.sleep(30)
+neko()
---
+++ tryjango/neko/nekoapp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class NekoappConfig(AppConfig):
+    name = 'nekoapp'
